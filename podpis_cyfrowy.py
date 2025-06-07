d62682cbfced13b1da86cc13c873c0e0b46c3c63
Maja <283884@student.pwr.edu.pl>
2025-06-07 10:13:35 +0200

    Komentarze

--- podpis_cyfrowy.py
+++ podpis_cyfrowy.py
@@ -1,12 +1,13 @@
 import sys
 from lightdsa import LightDSA
 
+#Prosimy uzytkownika o podanie sciezki do pliku i wybieramy algorytm
 file_path = input("Podaj ścieżkę do pliku, który chcesz podpisać:")
 dsa = LightDSA(algorithm_name="dsa")
 
+#Odczytujemy plik i tworzymy podpis cyfrowy
 with open (file_path, "r", encoding="utf-8") as f:
     message = f.read()
-
 signature = dsa.sign(message)
-
+#Wywolujemy podpis
 print (signature)
